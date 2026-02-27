32c5791deb73ecb6764805b2254ca90525401923
{given <{bibekrajtripathy@gmail.com}>
2026-02-27 11:09:21 +0530

    this is my firs commit on this file

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import dbConnect from "./config/db.js";
+import userRouter from "./routers/userRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.use("/api/users", userRouter);
+
+const startServer = async () => {
+  await dbConnect();
+
+  const PORT = process.env.PORT || 8080;
+
+  app.listen(PORT, () => {
+    console.log(`Server Started on port ${PORT}`);
+  });
+};
+
+startServer();
